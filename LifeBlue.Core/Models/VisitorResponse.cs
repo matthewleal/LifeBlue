582d7292d1ba4176d1d07bc482db34baad2dd2be
Matt Leal <mleal@republicfinance.com>
2023-10-01 21:54:17 -0500

    Adding Visitor Response Class

---
+++ LifeBlue.Core/Models/VisitorResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using LifeBlue.Dal.DTO;
+
+namespace LifeBlue.Core.Models
+{
+    public class VisitorResponse
+    {
+        public VisitorInformation VisitorInformation { get; set; }
+        [JsonIgnore]
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
